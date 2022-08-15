1af0f93a8d1ef9ad8c5ff7b3d73c14ca3bf24e72
garungamers17 <110324183+findsolgaming@users.noreply.github.com>
2022-08-16 02:02:56 +0530

    Utkarsh

---
+++ Assignment/Assets/Scripts/ThirdpersonMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System;
+using Collection;
+
+public class ThirdPersonClass : monBehavior
+{
+	public static string namr ;
+	public void int mwin()
+	{
+		Debug.Log("Hello........... I  ma Your First user ");
+
+		text.text = "Play Ludo And Win";
+
+		statenameController.cs = text,Token();
+
+
+	}
+
+	public void mainFunction()
+	{
+		Debug.Log("Inside  the Funcgtion");
+
+	}
+}
